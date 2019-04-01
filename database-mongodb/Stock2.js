711e386c6d469e5eb7140b6b70c41e512eaaa24d
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-04-01 08:24:42 -0700

    Set up seed and schema for Purchase History module
    - Using mongoDB and npm faker
    - Referenced top 100 stocks as a csv file
    - Symbols is the unique identifier among modules

---
+++ database-mongodb/Stock2.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = require('./index.js');
+mongoose.Promise = global.Promise;
+
+const purchaseSchema = new mongoose.Schema({
+  symbol: String,
+  purchase_id: Number,
+  name: String,
+  timeinforce: String,
+  submitted: Date,
+  status: String,
+  entered_quantity: Number,
+  filled: Date, 
+  filled_quantity: Number
+});
+
+const Purchase = mongoose.model('Purchases', purchaseSchema);
+
+module.exports = Purchase;
