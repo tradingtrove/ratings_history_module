581c2c5c4fa0239983e0801bb877f801871116d7
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-04-01 14:56:21 -0700

    Conform to airbnb styling

--- database-mongodb/index.js
+++ database-mongodb/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const mongoUri = 'mongodb://localhost/fec_ratings';
 
-mongoose.connect(mongoUri, { useNewUrlParser: true }); 
-const db = mongoose.connection; 
+mongoose.connect(mongoUri, { useNewUrlParser: true });
+const db = mongoose.connection;
 
 db.once('open', () => {
   console.log('Now connected to MongoDB called fec_ratings...');
