4bdc46376c495ffd2e731621fc9146e9a1e2ac2c
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-04-01 19:14:13 -0700

    Update npm run test script to reflect correct Jest directory, (testing)

--- testing/index.test.js
+++ testing/index.test.js
@@ -1,5 +1,10 @@
 const request = require('supertest');
-const app = require('../client/src/components/App');
+const express = require('express');
+const myApp = require('../client/src/components/App');
+const Stocks = require('../database-mongodb/Stock');
+
+const app = express();
+const PORT = 3001;
 
 //  https://hackernoon.com/api-testing-using-supertest-1f830ce838f1
 //  looks like an integration test
@@ -9,7 +14,7 @@ const app = require('../client/src/components/App');
  */
 describe('GET /api/ratings/:stockID', () => {
   it('respond with json containing rating data for one stock', (done) => {
-    request(app)
+    request(myApp)
       .get('/api/ratings/:stockID')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
@@ -22,10 +27,71 @@ describe('GET /api/ratings/:stockID', () => {
  */
 describe('GET /api/history/:stockID', () => {
   it('respond with json containing rating data for one stock', (done) => {
-    request(app)
+    request(myApp)
       .get('/api/history/:stockID')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, done);
   });
+});
+
+
+/**
+ * Testing whether seed.js was successful
+ */
+describe('Test the removeStock method', () => {
+    let stock;
+
+    const testDBURL = 'mongodb://127.0.0.1:27017/fec_ratings';
+
+    const testData = {
+        normalStock: {
+            symbol: 'TSTSTOCK',
+            name: 'Test Stock', 
+            recBuy: 4,
+            recHold: 2,
+        }
+    }
+
+    beforeAll(() => {
+        mongoose.connect(testDBURL);
+    });
+    beforeEach(() => {
+        stock = new StockModel(testData.normalStock);
+        return stock.save();
+    });
+    afterEach(() => {
+        Stocks
+          .deleteOne({ symbol: 'TSTSTOCK' })
+          .exec((err) => {
+            if (err) {
+                throw (err)
+            }
+          });
+    });
+    afterAll((done) => {
+        mongoose.disconnect(done);
+    });
+});
+
+describe('Seed stocks collection in fec_ratings database', () => {
+  it('respond with the symbol name as a String type', (done) => {
+    app.get('/api/ratings/AAPL', (req, res) => {
+      Stocks
+        .findOne({ symbol: 'TSTSTOCK' })
+        .exec((err, data) => {
+          if (err) {
+            res.status(500).send(err);
+            throw (err);
+          }
+          expect(data).toHaveProperty.(name, 'Test Stock');
+          done();
+          res.status(200).send(data);
+        });
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running at: http://localhost:${PORT}`);
 });
