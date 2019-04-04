8add8bf190cfb22d7687095f986e55e916e3112f
Vickie Huang <39073897+vh02@users.noreply.github.com>
2019-04-03 20:51:05 -0700

    Fix ESLint config to recognize Jest Tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,21 @@ module.exports = {
    "parserOptions": { "ecmaVersion": 6 } ,
    plugins: [
     'react',
+    'jest',
   ],
   env: {
     browser: true,
     commonjs: true,
     es6: true,
     node: true,
+    "jest/globals": true,
   },
+    "rules": {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error"
+    },
+  
 };
