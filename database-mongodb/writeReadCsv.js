0fc93a8ad5816a10f216fb40a5523e70b3b1036c
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-04-20 14:27:39 -0700

    Implement drain for purchase data

--- database-mongodb/writeReadCsv.js
+++ database-mongodb/writeReadCsv.js
@@ -1,11 +1,52 @@
 const fs = require('fs');
-const csv = require('fast-csv');
+const _ = require('lodash');
 const { generatePurchaseData, generateStocksData } = require('./seed');
 // const { Stock, Purchase, db } = require('./index');
 
 
+console.time('PURCHASE');
+
+
 const writePurchaseData = fs.createWriteStream('./database-mongodb/csvPurchaseData.csv');
 const writeStocksData = fs.createWriteStream('./database-mongodb/csvStocksData.csv');
+
+const header = [
+  'symbol', 'name', 'timeinforce', 'submitted', 'status', 'enteredQuantity',
+  'filled', 'filledQuantityShares', 'filledQuantityPrice', 'total',
+];
+writePurchaseData.write(`${header.join(',')}\n`);
+
+const writeTenMillionPurchaseData = (writer, encoding, callback) => {
+  let i = 1000000;
+  const write = () => {
+    let ok = true;
+    do {
+      i -= 1;
+      const purchasedTimes = _.random(1, 3);
+      for (let j = 0; j < purchasedTimes; j += 1) {
+        const purchaseData = generatePurchaseData();
+        const data = `${header.map(key => purchaseData[key]).join(',')}\n`;
+        if (i === 0) {
+          writer.write(data, encoding, callback);
+        } else {
+          ok = writer.write(data, encoding);
+        }
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  };
+  write();
+};
+
+writeTenMillionPurchaseData(writePurchaseData, 'UTF-8', () => console.timeEnd('PURCHASE'));
+
+// const createCsvStocksData = () => {
+//   csv.write(generateStocksData(), { headers: true })
+//     .pipe(writeStocksData);
+// };
+
 
 // const insertPurchaseData = (data) => {
 //   if (Purchase.collection) {
@@ -24,13 +65,6 @@ const writeStocksData = fs.createWriteStream('./database-mongodb/csvStocksData.c
 //     .then(() => db.close())
 //     .catch(err => console.log(`Error saving data to database: ${err}`));
 // };
-
-csv.write(generatePurchaseData(), { headers: true })
-  .pipe(writePurchaseData);
-
-csv.write(generateStocksData(), { headers: true })
-  .pipe(writeStocksData);
-
 
 // fs.createReadStream('./database-mongodb/csvPurchaseData.csv')
 //   .pipe(csv())
