719484f184b7898c27c3cc32545932f9dd7e7c47
Toshihiro Akabori <toshihiro.akabori@yahoo.com>
2019-04-21 15:52:55 -0700

    Optimize algorithm

--- database-mongodb/seed.js
+++ database-mongodb/seed.js
@@ -2,9 +2,56 @@ const faker = require('faker');
 const _ = require('lodash');
 
 
-const alphabet = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-];
+const generatePurchaseData = (symbol, name) => {
+  let onePurchaseData = {};
+  const purchaseDateRange = faker.date.between('2019-01-01', '2019-04-17');
+  const filledQuantity = faker.random.number(100);
+  const filledPrice = faker.random.number(50);
+  const totalPrice = filledQuantity * filledPrice;
+  const timeInForceOptions = [
+    'Day-only', 'Good-until-cancelled', 'Immediate-or-cancel',
+  ];
+
+  const purchaseData = {
+    symbol,
+    name,
+    timeinforce: timeInForceOptions[_.random(0, 2)],
+    submitted: purchaseDateRange,
+    status: 'Filled',
+    enteredQuantity: filledQuantity,
+    filled: faker.date.between(purchaseDateRange, '2019-04-17'),
+    filledQuantityShares: filledQuantity,
+    filledQuantityPrice: filledPrice,
+    total: totalPrice,
+  };
+  onePurchaseData = purchaseData;
+  return onePurchaseData;
+};
+
+const generateStocksData = (symbol) => {
+  let oneStockData = {};
+  const stockData = {
+    symbol,
+    recBuy: faker.random.number(20),
+    recHold: faker.random.number(20),
+    recSell: faker.random.number(20),
+    reviewBuy: faker.lorem.paragraph(),
+    reviewSell: faker.lorem.paragraph(),
+  };
+  oneStockData = stockData;
+  return oneStockData;
+};
+
+
+module.exports = {
+  generatePurchaseData,
+  generateStocksData,
+};
+
+
+// const alphabet = [
+//   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+// ];
 // const symbols = new Set();
 // const generateSymbolsAndNames = (() => {
 //   while (symbols.size < 1) {
@@ -18,69 +65,3 @@ const alphabet = [
 //     }
 //   }
 // })();
-const symbols = new Set();
-const generateSymbolsAndNames = (() => {
-  while (symbols.size < 1) {
-    const symbolLength = _.random(1, 5);
-    let currSymbol = '';
-    for (let i = 0; i < symbolLength; i += 1) {
-      currSymbol += alphabet[_.random(0, 25)];
-    }
-    if (symbols.has(currSymbol) === false) {
-      symbols.add(currSymbol);
-    }
-  }
-})();
-
-const generatePurchaseData = () => {
-  let onePurchaseData = {};
-  symbols.forEach((symbol) => {
-    const companyName = faker.company.companyName();
-    const purchaseDateRange = faker.date.between('2019-01-01', '2019-04-17');
-    const filledQuantity = faker.random.number(100);
-    const filledPrice = faker.random.number(50);
-    const totalPrice = filledQuantity * filledPrice;
-    const timeInForceOptions = [
-      'Day-only', 'Good-until-cancelled', 'Immediate-or-cancel',
-    ];
-
-    const purchaseData = {
-      symbol,
-      name: companyName,
-      timeinforce: timeInForceOptions[_.random(0, 2)],
-      submitted: purchaseDateRange,
-      status: 'Filled',
-      enteredQuantity: filledQuantity,
-      filled: faker.date.between(purchaseDateRange, '2019-04-17'),
-      filledQuantityShares: filledQuantity,
-      filledQuantityPrice: filledPrice,
-      total: totalPrice,
-    };
-    onePurchaseData = purchaseData;
-  });
-  return onePurchaseData;
-};
-
-const generateStocksData = () => {
-  const allStocksData = new Set();
-  const fillsStocksDataProperties = (() => {
-    symbols.forEach((symbol) => {
-      const oneStockData = {
-        symbol,
-        recBuy: faker.random.number(20),
-        recHold: faker.random.number(20),
-        recSell: faker.random.number(20),
-        reviewBuy: faker.lorem.paragraph(),
-        reviewSell: faker.lorem.paragraph(),
-      };
-      allStocksData.add(oneStockData);
-    });
-  })();
-  return Array.from(allStocksData);
-};
-
-
-module.exports = {
-  generatePurchaseData,
-  generateStocksData,
-};
--- database-mongodb/writeReadCsv.js
+++ database-mongodb/writeReadCsv.js
@@ -1,51 +1,98 @@
 const fs = require('fs');
 const _ = require('lodash');
+const faker = require('faker');
 const { generatePurchaseData, generateStocksData } = require('./seed');
 // const { Stock, Purchase, db } = require('./index');
-
-
-console.time('PURCHASE');
 
 
 const writePurchaseData = fs.createWriteStream('./database-mongodb/csvPurchaseData.csv');
 const writeStocksData = fs.createWriteStream('./database-mongodb/csvStocksData.csv');
 
-const header = [
+const purchaseHeader = [
   'symbol', 'name', 'timeinforce', 'submitted', 'status', 'enteredQuantity',
   'filled', 'filledQuantityShares', 'filledQuantityPrice', 'total',
 ];
-writePurchaseData.write(`${header.join(',')}\n`);
+writePurchaseData.write(`${purchaseHeader.join(',')}\n`);
 
-const writeTenMillionPurchaseData = (writer, encoding, callback) => {
-  let i = 1000000;
+const stocksHeader = [
+  'symbol', 'recBuy', 'recHold', 'recSell', 'reviewBuy', 'reviewSell',
+];
+writeStocksData.write(`${stocksHeader.join(',')}\n`);
+
+const alphabet = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+];
+const symbols = [];
+const generateSymbols = () => {
+  let word = '';
+  for (let firstChar = 0; firstChar < 26; firstChar += 1) {
+    for (let secondChar = 0; secondChar < 26; secondChar += 1) {
+      for (let thirdChar = 0; thirdChar < 26; thirdChar += 1) {
+        for (let fourthChar = 0; fourthChar < 26; fourthChar += 1) {
+          for (let fifthChar = 0; fifthChar < 26; fifthChar += 1) {
+            word = alphabet[firstChar] + alphabet[secondChar] + alphabet[thirdChar]
+            + alphabet[fourthChar] + alphabet[fifthChar];
+            symbols.push(word);
+          }
+        }
+      }
+    }
+  }
+};
+console.time('SYMBOLS');
+generateSymbols();
+console.timeEnd('SYMBOLS');
+
+const writeTenMillionPurchaseData = (writer, encoding, callback, tenMillion) => {
+  const names = [];
+  let i = 0;
   const write = () => {
     let ok = true;
     do {
-      i -= 1;
-      const purchasedTimes = _.random(1, 3);
+      names.push(faker.company.companyName());
+      const purchasedTimes = _.random(1, 5);
       for (let j = 0; j < purchasedTimes; j += 1) {
-        const purchaseData = generatePurchaseData();
-        const data = `${header.map(key => purchaseData[key]).join(',')}\n`;
-        if (i === 0) {
+        const purchaseData = generatePurchaseData(symbols[i], names[i]);
+        const data = `${purchaseHeader.map(key => purchaseData[key]).join(',')}\n`;
+        if (i === tenMillion) {
           writer.write(data, encoding, callback);
         } else {
           ok = writer.write(data, encoding);
         }
       }
-    } while (i > 0 && ok);
-    if (i > 0) {
+      i += 1;
+    } while (i < tenMillion && ok);
+    if (i < tenMillion) {
       writer.once('drain', write);
     }
   };
   write();
 };
+console.time('PURCHASE');
+writeTenMillionPurchaseData(writePurchaseData, 'UTF-8', () => console.timeEnd('PURCHASE'), 1000000);
 
-writeTenMillionPurchaseData(writePurchaseData, 'UTF-8', () => console.timeEnd('PURCHASE'));
-
-// const createCsvStocksData = () => {
-//   csv.write(generateStocksData(), { headers: true })
-//     .pipe(writeStocksData);
-// };
+const writeTenMillionStocksData = (writer, encoding, callback, tenMillion) => {
+  let i = 0;
+  const write = () => {
+    let ok = true;
+    do {
+      const stocksData = generateStocksData(symbols[i]);
+      const data = `${stocksHeader.map(key => stocksData[key]).join(',')}\n`;
+      if (i === tenMillion) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+      i += 1;
+    } while (i < tenMillion && ok);
+    if (i < tenMillion) {
+      writer.once('drain', write);
+    }
+  };
+  write();
+};
+console.time('STOCKS');
+writeTenMillionStocksData(writeStocksData, 'UTF-8', () => console.timeEnd('STOCKS'), 1000000);
 
 
 // const insertPurchaseData = (data) => {
